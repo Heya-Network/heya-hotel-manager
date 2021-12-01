32d282572761a77e9eb57ce28f3b78b8c0b92f4a
reigun <reinaldo@blocksphere.id>
2021-12-01 15:21:26 +0800

    Added more substrate info to root route and .toHuman() to free balance in substrate service

--- src/controllers/hotels/hotels.controller.ts
+++ src/controllers/hotels/hotels.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('hotels')
-export class HotelsController {}
+export class HotelsController {
+
+    @Get("/statuses/:ss58")
+    getStatusRoute(@Param("ss58") ss58: string): string {
+        return ss58;
+    }
+}
--- src/services/substrate.service.ts
+++ src/services/substrate.service.ts
@@ -10,12 +10,11 @@ export class SubstrateService {
   async getBalance(ss58: string): Promise<Object> {
     const {polkadotApi} = new PolkadotJs();
         const api = await polkadotApi(this.configService.get<string>("PROVIDER_SOCKET"));
-        // Retrieve the last timestamp
         // Retrieve last block timestamp, account nonce & balances
         const [now, { nonce, data: balance }] = await Promise.all([
           api.query.timestamp.now(),
           api.query.system.account(ss58)
         ]);
-        return {now, nonce, data: balance}
+        return {now, nonce, data: balance.toHuman()}
   }
 }
