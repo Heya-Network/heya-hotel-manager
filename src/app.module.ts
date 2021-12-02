5a3e6a5cbb1557c753b8d638a7020187794d18cc
reigun <reinaldo@blocksphere.id>
2021-12-02 11:59:01 +0800

    app.module: removed forRootAsync because mikroOrm config is now loaded from mikro-orm.config.ts & .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { BalancesController } from 'controllers/balances/balances.controller';
-import { RootController } from 'controllers/root/root.controller';
-import { SubstrateService } from 'services';
+import { RootController, HotelsController, BalancesController } from 'controllers';
+import { HotelsService, SubstrateService } from 'services';
 
 @Module({
   imports: [
@@ -12,27 +10,10 @@ import { SubstrateService } from 'services';
       isGlobal: true,
       cache: true,
     }),
-    MikroOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        entities: ['dist/entities'],
-        entitiesTs: ['src/entities'],
-        inject: [ConfigService],
-        baseDir: process.cwd(),
-        discovery: {
-          warnWhenNoEntities: true, // by default, discovery throws when no entity is processed
-          requireEntitiesArray: false, // force usage of class refrences in `entities` instead of paths
-          alwaysAnalyseProperties: false, // do not analyse properties when not needed (with ts-morph)
-        },
-        dbName: 'admin',
-        driver: PostgreSqlDriver,
-        type: 'postgresql', // one of `mongo` | `mysql` | `mariadb` | `postgresql` | `sqlite`
-        // clientUrl: "postgresql://"+configService.get<string>("DB_USER")+'@'+configService.get<string>("DB_ADDR")+':'+configService.get<string>("DB_PORT") // defaults to 'mongodb://localhost:27017' for mongodb driver
-      }),
-    }),
+    MikroOrmModule.forRoot(),
   ],
   
-  controllers: [RootController, BalancesController],
-  providers: [SubstrateService],
+  controllers: [RootController, BalancesController, HotelsController],
+  providers: [SubstrateService, HotelsService],
 })
 export class AppModule {}
